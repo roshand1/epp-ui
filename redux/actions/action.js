87d53659a60729b4d819e95f2ad686d89721d1bc
roshand1 <dangal.roshan@gmail.com>
2018-08-09 14:23:57 -0600

    Update action.js

--- redux/actions/action.js
+++ redux/actions/action.js
@@ -3,7 +3,7 @@ import _fetch from '../../utils/HelperFetch.js';
 
 export function getPracticeModel(){
     return function(dispatch){
-        _fetch('https://www.healthgrades.com/uisvc/v1_0/eppuiservice/api/Provider/GetViewModel?officeId=oo65fmp',{method:'GET'},function(status, practiceModel){
+        _fetch('someurll?officeId=oo65fmp',{method:'GET'},function(status, practiceModel){
             if(status =='OK'){
                dispatch({type:'FETCH_PRAC_MODEL_SUCCESS',payload:practiceModel});
                 var event = new CustomEvent('displayAds', { 'detail': {AdModel:practiceModel.Adds,OmniturePageTracking:practiceModel.OmniturePageTracking} });
@@ -23,7 +23,7 @@ export function getPracticeModel(){
 export function getProvidersByPracticeId(practiceId, skip, take){
 const uri = '?practiceId=' + practiceId +'&skip='+skip+'&take='+take;
     return function(dispatch){
-        _fetch('https://www.healthgrades.com/uisvc/v1_0/eppuiservice/api/Provider/GetProviders'+uri,{method:'GET'},function(status, response){
+        _fetch('https:someruslider/GetProviders'+uri,{method:'GET'},function(status, response){
             if(status == 'OK'){
                 dispatch({type:'FETCH_PROV_SUCCESS',payload:response});
             }
